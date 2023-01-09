f64a004d2e702c8aa307711b1c673b067e21ee0a
Arsi <121026633+Arsidead@users.noreply.github.com>
2023-01-09 11:56:19 +0100

    Add files via upload

--- GroupProject.Arsenii.py
+++ GroupProject.Arsenii.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 *-*
 
 from PIL import Image
+import numpy as np
+im = Image.open("images project.jpg")
+im.show()
 
-def import(f):
-    dfghdfk
-    tgh
 
-var=[]
-def affi(var):
-    var.append(import(im))
+im_new = Image.new("RGB", (512, 504), (128, 128, 128))
+largeur, hauteur = im.size
+
+image = np.asarray(im_new)
+nb_lignes,nb_colonnes,_ = image.shape
+
+image_sortie = np.copy(im)
+
+Image.fromarray(image).save("image_entree.jpg")
+Image.fromarray(image_sortie).save("image_sortie.jpg")
+
+image_entrée = Image.open("images project.jpg")
+image = np.asarray(im)
+
+print(image)
+
+Image.fromarray(image).save("output.png")
+
+
+
+
+
+
+
